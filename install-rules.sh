e7641a4aa4c169d5dd4b3d16586a9e892b1fedbb
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2023-02-11 11:21:25 +0000

    Add files via upload

--- install-rules.sh
+++ install-rules.sh
@@ -5,5 +5,10 @@ cat > /etc/udev/rules.d/70-slab-generic.rules << EOF
 SUBSYSTEM=="usb", ATTRS{idVendor}=="10c4", MODE="0666"
 SUBSYSTEM=="usb_device", ATTRS{idVendor}=="10c4", MODE="0666"
 EOF
-service udev restart
+services=$(service --status-all)
+if echo $services | grep -q eudev; then
+    service eudev restart
+elif echo $services | grep -q udev; then
+    service udev restart
+fi
 echo Done!
