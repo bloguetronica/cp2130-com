92d2d64cca8f6f1f2a29f96b3a08537974967caf
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2025-09-14 16:36:11 +0100

    Add files via upload

--- src/delaysdialog.h
+++ src/delaysdialog.h
@@ -1,5 +1,5 @@
-/* CP2130 Commander - Version 5.0 for Debian Linux
-   Copyright (c) 2022-2024 Samuel Lourenço
+/* CP2130 Commander - Version 1.5.1 for Debian Linux
+   Copyright (c) 2022-2025 Samuel Lourenço
 
    This program is free software: you can redistribute it and/or modify it
    under the terms of the GNU General Public License as published by the Free
